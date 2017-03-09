7861c2f7bc0464eb863d40f19058c224c00e345e
Ellen Marshall <lnor.mars@gmail.com>
2017-03-08 22:02:40 -0500

    linked reviews.json and loop through reviews to display in respond section.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var bodyParser = require('body-parser)');
+var bodyParser = require('body-parser');
 
 var app = express();
 var port = 3000;
@@ -8,22 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-var reviews = [
-  {
-     "review": {
-    "id": 1,
-    "reviewer": {
-      "id": 123,
-      "name": "Jessie"
-    },
-    "source": "Yelp",
-    "rating": 4,
-    "text": "AWESOME PLACE!  They didn't even kick my children.",
-    "time-created": "2017-02-28 13-46-24",
-    "url": "http://fugeziLink.com/reviews/id:1"
-    }
-  }
-];
+var reviews = require('./reviews.json')
 
 app.get('/reviews', function(req, res) {
   res.json(reviews);
--- public/main.js
+++ public/main.js
@@ -1,9 +1,26 @@
+function review(source, rating, dateCreated, text, url) {
+  var source = $("<p></p>").text(source);
+  var rating = $("<p></p>").text(rating);
+  var date = $("<p></p>").text(dateCreated);
+  var content = $("<p></p>").text(text);
+  var link = $("<a target='_blank'>View on" + source + "</a>").attr("href", url);
+
+  var reviewContent = $("<div class='review'>").append(source, rating, date, content, link);
+
+  console.log(source, rating, date);
+
+  return reviewContent;
+};
+
 $(document).ready(function() {
 
   $.get('/reviews', function(reviews) {
-    };
+ //   var loop = reviews[i].review;
+
+    for (var i in reviews) {
+      $(".list-existing-reviews").append(review(reviews[i].review.source, reviews[i].review.rating, reviews[i].review.dateCreated, reviews[i].review.text, reviews[i].review.url));
+      console.log(reviews[i].review.source);
+    }
   });
-
-
 
 });
